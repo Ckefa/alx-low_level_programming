c1342a778533d791bcfca8842168645e873adf58
ckefa <ckefa65@gmail.com>
2022-09-14 09:10:52 -0700

    ghu

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- *putchar
- *am increasing the size of description
- */
+*putchar
+*am increasing the size of description
+*/
 int main(void)
+//main function
 {
 printf("_putchar\n");
+//return 0
 return(0);
 }
+
