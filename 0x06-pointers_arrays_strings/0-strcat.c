635ba7dda8c6681b2e9497947254e7f689a14527
ckefa <ckefa65@gmail.com>
2022-09-22 00:07:30 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - The main function of the program
+ * it controls other functions
+ *
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src);
+int main(void)
+{
+char s1[98] = "Hello ";
+char s2[] = "World!\n";
+char *ptr;
+
+printf("%s\n", s1);
+printf("%s", s2);
+
+ptr = _strcat(s1, s2);
+
+printf("%s", s1);
+printf("%s", s2);
+printf("%s", ptr);
+
+return (0);
+}
+/**
+ * _strcat - this function concats two string
+ *
+ * @dest: destination string; src string is appended to it
+ *
+ * @src: Source string; gets appended to dest string
+ *
+ * Return: Concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
+
